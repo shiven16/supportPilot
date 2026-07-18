6f0782d007c1fd6c998344b2d909655a88190278
shiven16 <shivenahuja2004@gmail.com>
2026-07-18 08:13:42 +0530

    fix: update healthcheck endpoint for aws alb

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/health')
+  @Get(['/health', '/api/health'])
   getHello(): string {
     return this.appService.getHello();
   }
